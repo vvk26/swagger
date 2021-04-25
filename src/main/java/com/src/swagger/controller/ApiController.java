a9252314c438496d3cfc11a7a038e7641081b19a
Kadam <VivekanandM.Kadam@centurylink.com>
2021-04-25 21:18:08 +0530

    adding project

---
+++ src/main/java/com/src/swagger/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.src.swagger.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ApiController {
+
+    @CrossOrigin
+    @ResponseBody
+    @GetMapping(value = "/swaggerGet")
+    public ResponseEntity<?> swaggerTest() {
+        return ResponseEntity.ok().body("Swagger for Get call working fine");
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @PostMapping(value = "/swaggerPost")
+    public ResponseEntity<?> swaggerPost(@RequestParam(value = "test",required = false) String test) {
+        return ResponseEntity.ok().body("Swagger for Post call working fine : "+ test);
+    }
+}
+
